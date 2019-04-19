93c5876403f725cc300d6da91fc8ec708b229116
eakarpov <eakarpov@yandex.ru>
2019-04-19 14:04:36 +0300

    page limit

--- src/queryBuilder/index.ts
+++ src/queryBuilder/index.ts
@@ -1,10 +1,26 @@
 export interface IQuery {
     page: number;
+    skip: number;
     limit: number;
 }
 
 export class QueryBuilder {
+    public static defaultLimit = 50;
+
     public static build(queryObject: IQuery): string {
-        return '';
+        let res = '';
+        const arr = [];
+        if (queryObject.page || queryObject.skip) {
+            if (queryObject.skip) {
+                arr.push(`$skip=${queryObject.skip}`);
+            } else {
+                arr.push(`$skip=${queryObject.page * (queryObject.limit || QueryBuilder.defaultLimit)}`);
+            }
+        }
+        if (queryObject.limit) {
+            arr.push(`$top=${queryObject.limit}`);
+        }
+        res += arr.join(' and ');
+        return res;
     }
 }
